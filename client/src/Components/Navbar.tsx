31fdf6eaf5146619ca698d3ac431a6cf1790b1ae
Jongmun-Park <momenttraveler@gmail.com>
2020-11-10 19:01:31 +0900

    헤더 레이아웃 업데이트

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -3,26 +3,38 @@ import { useQuery } from '@apollo/react-hooks'
 
 import React, { FunctionComponent, useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { AppBar, Button, Toolbar } from '@material-ui/core'
+import { AppBar, Button, Grid, Toolbar } from '@material-ui/core'
 
 import SignUp from './User/SignUp'
 import Login from './User/Login'
 
 const useStyles = makeStyles(() => ({
-  navbar: {
-    width: '100%',
-    '& >div': { float: 'right' },
-    '& >ul': {
-      margin: 0,
-      listStyle: 'none',
-      '& >li': { float: 'left' },
-    },
-    '& a': {
-      textDecoration: 'none',
-      padding: '10px 20px',
-      '&:hover': { color: '#B22222' },
-    },
+  container: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    height: '60px',
+    backgroundColor: 'rgb(255, 255, 255)',
+    borderBottom: '1px solid rgb(229, 229, 229)',
+    // '& a': {
+    //   textDecoration: 'none',
+    //   padding: '10px 20px',
+    //   '&:hover': { color: '#B22222' },
+    // },
   },
+  headerLogo: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginLeft: '20px'
+  },
+  headerButtons: {
+    display: 'flex',
+    width: '400px',
+    marginRight: '20px',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  searchBox: {},
 }))
 
 const IS_LOGIN = gql`
@@ -55,50 +67,53 @@ const NavBar: FunctionComponent = () => {
   }
 
   return (
-    <AppBar color="inherit" position="static">
-      <Toolbar>
-        <a href="/">
-          <img
-            alt="Brand Logo"
-            style={{ width: '50px', height: '50px', verticalAlign: 'middle' }}
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Artsy_logo.svg/1198px-Artsy_logo.svg.png"
-          ></img>
-        </a>
-        <nav className={classes.navbar}>
-          <ul>
-            <li>
-              <a href="/art">Art</a>
-            </li>
-            <li>
-              <a href="/practice">Practice</a>
-            </li>
-          </ul>
+    <header>
+      <nav className={classes.container}>
+        <div className={classes.headerLogo}>
+          <div style={{ height: '45px' }}>
+            <a href="/" style={{ display: 'block' }}>
+              <img
+                alt="Header Logo"
+                style={{ width: '45px', height: '45px' }}
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Artsy_logo.svg/1198px-Artsy_logo.svg.png"
+              ></img>
+            </a>
+          </div>
+        </div>
+        <div className={classes.searchBox}></div>
+        <div className={classes.headerButtons}>
+          <Button href="/art">ART</Button>
+          <Button href="/artist">ARTIST</Button>
           {!data?.currentUser ? (
             <div>
-              <Button variant="outlined" color="primary" onClick={logInClickOpen}>
-                로그인
+              <Button variant="outlined" onClick={logInClickOpen}>
+                LOG IN
               </Button>
               <Button
                 variant="outlined"
-                color="primary"
                 onClick={signUpClickOpen}
                 style={{ marginLeft: '15px' }}
               >
-                회원 가입
+                SIGN UP
               </Button>
               {signUpOpen && <SignUp openDialog={signUpOpen} handleOpenDialog={setSignUpOpen} />}
               {logInOpen && <Login openDialog={logInOpen} handleOpenDialog={setLogInOpen} />}
             </div>
           ) : (
             <div>
-              <Button variant="outlined" color="primary" href="/upload-post">
-                글쓰기
+              <Button variant="outlined" href="/upload-post">
+                LOG OUT
               </Button>
+              {/* <Button variant="outlined" href="/upload-post">
+                UPLOAD
+              </Button> */}
             </div>
           )}
-        </nav>
-      </Toolbar>
-    </AppBar>
+        </div>
+      </nav>
+    </header>
+    //   </Toolbar>
+    // </AppBar>
   )
 }
 
