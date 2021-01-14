2dfa4d62f226790181e53ef15a6838d905db058c
Jongmun-Park <momenttraveler@gmail.com>
2021-01-14 17:22:35 +0900

    WIP: add profile image preview

--- client/src/Components/User/RegisterArtist.tsx
+++ client/src/Components/User/RegisterArtist.tsx
@@ -1,16 +1,11 @@
 import React, { FunctionComponent, ChangeEvent, useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { Typography, TextField, Button } from '@material-ui/core'
+import { Avatar, Typography, TextField, Button, TextareaAutosize } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import gql from 'graphql-tag'
 import { useMutation } from '@apollo/react-hooks'
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
   centerArea: {
     height: '100vh',
     maxWidth: '1100px',
@@ -20,6 +15,18 @@ const useStyles = makeStyles((theme) => ({
   section: {
     // TODO: 모바일 스타일 적용
     margin: '50px 80px 50px 80px',
+  },
+  inputContainer: {
+    maxWidth: '400px',
+    margin: '30px 30px 30px 60px',
+  },
+  input: {
+    width: '100%',
+    marginBottom: '20px',
+  },
+  largeAvatar: {
+    width: theme.spacing(20),
+    height: theme.spacing(20),
   },
 }))
 
@@ -38,11 +45,25 @@ const UPLOAD_POST_MUTATION = gql`
 
 const RegisterArtist: FunctionComponent = () => {
   const classes = useStyles({})
+  const [imgBase64, setImgBase64] = useState('')
   const [createPost] = useMutation(UPLOAD_POST_MUTATION)
   const [inputs, setInputs] = useState<UploadPostInputProps>({
     title: '',
     thumbnail: null,
   })
+
+  const handleChangeThumbnail = (e: ChangeEvent<HTMLInputElement>) => {
+    let reader = new FileReader()
+    if (e.target.files?.[0]) {
+      reader.readAsDataURL(e.target.files?.[0])
+      reader.onload = () => {
+        const base64 = reader.result
+        if (base64) {
+          setImgBase64(base64.toString())
+        }
+      }
+    }
+  }
 
   const handleUpload = async () => {
     console.log('inputs.thumbnail :', inputs.thumbnail)
@@ -84,73 +105,68 @@ const RegisterArtist: FunctionComponent = () => {
 
   return (
     <div className={classes.centerArea}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField
-          name="artistName"
-          label="필명(닉네임)"
-          variant="outlined"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          inputRef={register({
-            required: '필명(닉네임)을 입력해주세요.',
-            maxLength: {
-              value: 32,
-              message: '필명(닉네임)은 32자 이내로 입력해주세요.',
-            },
-          })}
-        />
-        {/* <input type="text" placeholder="필명(닉네임)" name="artistName" ref={register({required: true, maxLength: 64})} /> */}
-        <input
-          type="text"
-          placeholder="성명"
-          name="realName"
-          ref={register({ required: true, maxLength: 32 })}
-        />
-        <input
-          type="tel"
-          placeholder="휴대전화 번호"
-          name="phone"
-          ref={register({ required: true, minLength: 10, maxLength: 11 })}
-        />
-        {/* <select name="Title" ref={register({ required: true })}>
-        <option value="Mr">Mr</option>
-        <option value="Mrs">Mrs</option>
-        <option value="Miss">Miss</option>
-        <option value="Dr">Dr</option>
-      </select>
-
-      <input name="Developer" type="radio" value="Yes" ref={register({ required: true })}/>
-      <input name="Developer" type="radio" value="No" ref={register({ required: true })}/> */}
-
-        <input type="submit" />
-      </form>
-      <Typography variant="subtitle1">제 목</Typography>
-      <TextField
-        autoFocus
-        margin="dense"
-        type="text"
-        name="title"
-        value={inputs.title}
-        onChange={handleValueChange}
-        fullWidth
-      />
-      <Typography variant="subtitle1">썸네일 이미지 선택</Typography>
-      <input
-        type="file"
-        name="thumbnail"
-        accept="image/*"
-        onChange={handleFileChange}
-        style={{ marginTop: '10px' }}
-      ></input>
-      <Button
-        color="primary"
-        variant="contained"
-        onClick={handleUpload}
-        style={{ marginTop: '25px', float: 'right' }}
-      >
-        등록하기
-      </Button>
+      <h3>작가 등록</h3>
+      <div className={classes.inputContainer}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            className={classes.input}
+            autoFocus
+            name="artistName"
+            label="필명(닉네임)"
+            placeholder="필명(닉네임)"
+            variant="outlined"
+            required={true}
+            inputRef={register({
+              maxLength: {
+                value: 32,
+                message: '필명(닉네임)은 32자 이내로 입력해주세요.',
+              },
+            })}
+          />
+          <TextField
+            className={classes.input}
+            name="realName"
+            label="성명"
+            placeholder="성명"
+            variant="outlined"
+            required={true}
+            inputRef={register({
+              maxLength: {
+                value: 32,
+                message: '성명은 32자 이내로 입력해주세요.',
+              },
+            })}
+          />
+          <TextField
+            className={classes.input}
+            name="phone"
+            type="tel"
+            label="휴대전화 번호"
+            placeholder="휴대전화 번호"
+            variant="outlined"
+            required={true}
+            inputRef={register({
+              minLength: {
+                value: 10,
+                message: '휴대전화 번호는 10자 이상으로 입력해주세요.',
+              },
+            })}
+          />
+          <TextareaAutosize
+            className={classes.input}
+            rowsMin={5}
+            placeholder="작가를 소개하는 글입니다. 작가로서 가치관, 작품 세계 등 본인의 생각을 자유롭게 표현해주세요."
+            // label="본인 소개"
+            // variant="outlined"
+          />
+          <Typography variant="subtitle1">프로필 이미지</Typography>
+          <input type="file" name="thumbnail" accept="image/*" onChange={handleChangeThumbnail} />
+          <Avatar src={imgBase64} className={classes.largeAvatar} />
+          <Button style={{ float: 'right' }} type="submit" color="primary" variant="contained">
+            등록하기
+          </Button>
+        </form>
+      </div>
     </div>
   )
 }
