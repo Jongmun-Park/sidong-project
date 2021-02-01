d337d5054b895d873e89956fc6f84f13c530f88f
Jongmun-Park <momenttraveler@gmail.com>
2021-02-01 17:34:59 +0900

    add setLastArtistId

--- client/src/Routes/Artist/ArtistListContainer.tsx
+++ client/src/Routes/Artist/ArtistListContainer.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect, useState } from 'react'
 import { useQuery, useLazyQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 import ArtistListPresenter from './ArtistListPresenter'
@@ -24,15 +24,18 @@ const ARTISTS = gql`
 
 const ArtistList: FunctionComponent = () => {
   const { data, error } = useQuery(ARTISTS)
-  const [loadMoreArtist, { data: newData, error: newError }] = useLazyQuery(ARTISTS)
-  const handleLoadMore = () => {
-    console.log('handleLoadMore')
-    loadMoreArtist({
-      variables: {
-        lastArtistId: 11,
-      },
-    })
-  }
+  const [loadMoreArtist, { data: fetchedData, error: fetchError }] = useLazyQuery(ARTISTS)
+  const [lastArtistId, setLastArtistId] = useState('')
+
+  useEffect(() => {
+    console.log('useEffect')
+    if (fetchedData) {
+      const fetchedArtists = fetchedData.artists
+      setLastArtistId(fetchedArtists[fetchedArtists.length - 1].id)
+    } else if (data) {
+      setLastArtistId(artists[artists.length - 1].id)
+    }
+  }, [data, fetchedData])
 
   if (!data) {
     return null
@@ -40,7 +43,24 @@ const ArtistList: FunctionComponent = () => {
   if (error) {
     console.error(error.message)
   }
+  if (fetchError) {
+    console.error(fetchError.message)
+  }
+
   const { artists } = data
+
+  const handleLoadMore = () => {
+    console.log('handleLoadMore')
+    loadMoreArtist({
+      variables: {
+        lastArtistId: lastArtistId,
+      },
+    })
+  }
+
+  console.log('data:', data)
+  console.log('lastArtistId:', lastArtistId)
+  console.log('fetchedData:', fetchedData)
 
   return (
     <>
