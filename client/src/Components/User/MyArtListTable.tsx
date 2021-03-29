f649ccfcb2321cdcd7e37f1d5f32483d629b8402
Jongmun-Park <momenttraveler@gmail.com>
2021-03-29 12:13:53 +0900

    Update MyArtListTable

--- client/src/Components/User/MyArtListTable.tsx
+++ client/src/Components/User/MyArtListTable.tsx
@@ -1,13 +1,19 @@
-import React, { ChangeEvent, FC, useState } from 'react'
+import React, { FC, useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableContainer from '@material-ui/core/TableContainer'
-import TableHead from '@material-ui/core/TableHead'
-import TablePagination from '@material-ui/core/TablePagination'
-import TableRow from '@material-ui/core/TableRow'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from '@material-ui/core'
+import { currencyFormatter, translateSaleStatus } from '../../utils'
+import { Art } from '../../types'
 
 const useStyles = makeStyles({
   root: {
@@ -18,72 +24,41 @@ const useStyles = makeStyles({
   },
 })
 
-interface Column {
-  id: 'name' | 'code' | 'population' | 'size' | 'density'
-  label: string
-  minWidth?: number
-  align?: 'right'
-  format?: (value: number) => string
-}
-
-const columns: Column[] = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toFixed(2),
-  },
-]
-
-interface Data {
-  name: string
-  code: string
-  population: number
-  size: number
-  density: number
-}
-
-function createData(name: string, code: string, population: number, size: number): Data {
-  const density = population / size
-  return { name, code, population, size, density }
-}
-
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-]
+const CURRENT_USER_ARTS = gql`
+  query CurrentUserArtsOffsetBased($page: Int, $pageSize: Int) {
+    currentUserArtsOffsetBased(page: $page, pageSize: $pageSize) {
+      totalCount
+      arts {
+        id
+        createdAt
+        name
+        saleStatus
+        price
+      }
+    }
+  }
+`
 
 const MyArtListTable: FC = () => {
   const classes = useStyles()
-  const [page, setPage] = useState(0)
-  const [totalCount, setTotalCount] = useState(30)
+  const [page, setPage] = useState<number>(0)
+  const [totalCount, setTotalCount] = useState<number>(30)
+  const [arts, setArts] = useState<Array<Art> | null>(null)
 
-  const handleChangePage = (event: unknown, newPage: number) => {
+  const { data, refetch } = useQuery(CURRENT_USER_ARTS, {
+    onCompleted: (data) => {
+      setArts(data.currentUserArtsOffsetBased.arts)
+      setTotalCount(data.currentUserArtsOffsetBased.totalCount)
+    },
+    onError: (error) => console.error(error.message),
+  })
+
+  if (!data) {
+    return null
+  }
+
+  const handleChangePage = (e: unknown, newPage: number) => {
+    refetch({ page: newPage })
     setPage(newPage)
   }
 
@@ -93,32 +68,26 @@ const MyArtListTable: FC = () => {
         <Table aria-label="나의 작품 목록" size="small" stickyHeader>
           <TableHead>
             <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
+              <TableCell align="center">ID</TableCell>
+              <TableCell align="center">등록일</TableCell>
+              <TableCell align="center">작품명</TableCell>
+              <TableCell align="center">가격</TableCell>
+              <TableCell align="center">판매 상태</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => {
-              return (
-                <TableRow hover tabIndex={-1} key={row.code}>
-                  {columns.map((column) => {
-                    const value = row[column.id]
-                    return (
-                      <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === 'number' ? column.format(value) : value}
-                      </TableCell>
-                    )
-                  })}
-                </TableRow>
-              )
-            })}
+            {arts &&
+              arts.map((art: Art) => {
+                return (
+                  <TableRow hover tabIndex={-1} key={art.id}>
+                    <TableCell>{art.id}</TableCell>
+                    <TableCell>{art.createdAt}</TableCell>
+                    <TableCell>{art.name}</TableCell>
+                    <TableCell>{currencyFormatter(art.price)}</TableCell>
+                    <TableCell>{translateSaleStatus(art.saleStatus)}</TableCell>
+                  </TableRow>
+                )
+              })}
           </TableBody>
         </Table>
       </TableContainer>
