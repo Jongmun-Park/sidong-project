8b736fdbd90c57cf0d78fd4e81ea074f2910bff0
Jongmun-Park <momenttraveler@gmail.com>
2021-06-13 16:59:28 +0900

    작품 보증서 양식 다운로드 버튼 추가

--- client/src/Components/Artist/MySaleListTable.tsx
+++ client/src/Components/Artist/MySaleListTable.tsx
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 import {
+  Button,
   Paper,
   Table,
   TableBody,
@@ -12,19 +13,24 @@ import {
   TablePagination,
   TableRow,
 } from '@material-ui/core'
+import GetAppIcon from '@material-ui/icons/GetApp'
 import { Order } from '../../types'
 import MySaleListTableRow from './MySaleListTableRow'
 
 const useStyles = makeStyles({
   root: {
     width: '100%',
-    marginTop: '20px',
-    '@media (max-width: 834px)': {
-      marginTop: '10px',
-    },
+    // marginTop: '20px',
+    // '@media (max-width: 834px)': {
+    //   marginTop: '10px',
+    // },
   },
   container: {
     maxHeight: '470px',
+  },
+  downloadButton: {
+    float: 'right',
+    marginBottom: '15px',
   },
 })
 
@@ -72,6 +78,18 @@ const MySaleListTable: FC = () => {
 
   return (
     <Paper className={classes.root}>
+      <Button
+        className={classes.downloadButton}
+        startIcon={<GetAppIcon />}
+        variant="outlined"
+        color="secondary"
+        onClick={() => {
+          window.location.href =
+            'https://s3.ap-northeast-2.amazonaws.com/assets.storage.jakupsil.co.kr/sample/%E1%84%8C%E1%85%A1%E1%86%A8%E1%84%91%E1%85%AE%E1%86%B7+%E1%84%87%E1%85%A9%E1%84%8C%E1%85%B3%E1%86%BC%E1%84%89%E1%85%A5+%E1%84%8B%E1%85%A3%E1%86%BC%E1%84%89%E1%85%B5%E1%86%A8.xlsx'
+        }}
+      >
+        작품보증서 양식
+      </Button>
       <TableContainer className={classes.container}>
         <Table aria-label="나의 판매 목록" size="small" stickyHeader>
           <TableHead>
